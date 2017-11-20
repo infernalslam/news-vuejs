1372970f1ae7b658fe7a8551f59460dc85456932
infernalslam <infernal-slam@hotmail.com>
2017-11-20 17:39:08 +0700

    fix code

---
+++ src/components/header/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+  <section class="section">
+    <div class="container">
+      <h1 class="title"> Hello News!  📝</h1>
+      <p class="subtitle">
+        Access worldwide <strong> news programmatically </strong>!
+      </p>
+    </div>
+  </section>
+  </div>
+</template>
---
+++ src/components/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+  <hearder-view />
+  <br> --------- <br>
+  <p v-for="(item, index) in list" :key="index">
+    Line:
+    <span v-text="item"></span>
+  </p>
+   <infinite-loading @infinite="infiniteHandler"></infinite-loading>
+  </div>
+</template>
+
+<script>
+import HearderView from './header/index'
+import InfiniteLoading from 'vue-infinite-loading'
+export default {
+  name: 'index',
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    infiniteHandler ($state) {
+      setTimeout(() => {
+        const temp = []
+        for (let i = this.list.length + 1; i <= this.list.length + 20; i++) {
+          temp.push(i)
+        }
+        this.list = this.list.concat(temp)
+        $state.loaded()
+      }, 3000)
+    }
+  },
+  components: {
+    HearderView,
+    InfiniteLoading
+  }
+}
+</script>
